c53165e8d1788056bb987f2076085931a6e6c41c
himetik <himetik@tutamail.com>
2024-12-26 15:04:01 +0300

    .

--- web/app/logging.py
+++ web/app/logging.py
@@ -2,15 +2,15 @@ from loguru import logger
 import os
 
 
-def setup_logging():
-    LOG_DIR = "logs"
-    os.makedirs(LOG_DIR, exist_ok=True)
+LOG_DIR = "logs"
+os.makedirs(LOG_DIR, exist_ok=True)
 
-    logger.add(
-        os.path.join(LOG_DIR, "errors.log"),
-        level="ERROR",
-        format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
-        rotation="10 MB",
-        retention="1 days",
-        compression="zip"
-    )
+
+logger.add(
+    os.path.join(LOG_DIR, "errors.log"),
+    level="ERROR",
+    format="{time:YYYY-MM-DD HH:mm:ss} | {level} | {message}",
+    rotation="10 MB",
+    retention="1 days",
+    compression="zip"
+)
