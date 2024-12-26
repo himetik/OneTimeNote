f5ae0c2c580a4bbccc09dc67c9184664c9986f6a
himetik <himetik@tutamail.com>
2024-12-26 15:10:43 +0300

    Add logging

--- web/app/application.py
+++ web/app/application.py
@@ -1,12 +1,18 @@
 import sys
 from web.app.factory import make_app
+from loguru import logger
 
 
-app = make_app()
+try:
+    app = make_app()
+except Exception as error:
+    logger.critical(f"Critical error during application initialization: {error}")
+    sys.exit(1)
 
 
 if __name__ == "__main__":
     try:
         app.run(host="0.0.0.0", port=8000)
-    except Exception as _:
+    except Exception as error:
+        logger.critical(f"Critical error while running the application: {error}")
         sys.exit(1)
