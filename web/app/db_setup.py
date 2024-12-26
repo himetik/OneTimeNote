588ee1bcf641246a2c2bb55d340eb71da336e464
himetik <himetik@tutamail.com>
2024-12-26 14:44:10 +0300

    Add get_database_url error handlers

--- web/app/db_setup.py
+++ web/app/db_setup.py
@@ -3,15 +3,21 @@ from dotenv import load_dotenv
 
 
 def get_database_url():
-    load_dotenv()
+    try:
+        load_dotenv()
 
-    user = os.getenv("POSTGRES_USER")
-    password = os.getenv("POSTGRES_PASSWORD")
-    host = os.getenv("POSTGRES_HOST", "database")
-    port = os.getenv("POSTGRES_PORT", 5432)
-    db_name = os.getenv("POSTGRES_DB")
+        user = os.getenv("POSTGRES_USER")
+        password = os.getenv("POSTGRES_PASSWORD")
+        host = os.getenv("POSTGRES_HOST", "database")
+        port = os.getenv("POSTGRES_PORT", 5432)
+        db_name = os.getenv("POSTGRES_DB")
 
-    return f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}"
+        if not all([user, password, db_name]):
+            raise ValueError("Missing required environment variables: POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_DB.")
+
+        return f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{db_name}"
+    except Exception:
+        return None
 
 
 DATABASE_URL = get_database_url()
