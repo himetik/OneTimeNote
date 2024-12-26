f2510ecf61db00566cfe09a27115f44e55f74722
himetik <himetik@tutamail.com>
2024-12-26 15:08:32 +0300

    .

--- web/app/wsgi.py
+++ web/app/wsgi.py
@@ -1,5 +1,10 @@
 from web.app.application import app
+from loguru import logger
 
 
 if __name__ == "__main__":
-    app.run()
+    try:
+        app.run()
+    except Exception as error:
+        logger.critical(f"Critical error while running the application: {error}")
+        raise
