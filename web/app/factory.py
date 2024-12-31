cbe760259f78a760a1b7b99f97b7176e358722dc
himetik <himetik@tutamail.com>
2024-12-31 11:56:17 +0300

    Add requests limiter

--- web/app/factory.py
+++ web/app/factory.py
@@ -2,6 +2,8 @@ import os
 import sys
 from flask import Flask
 from loguru import logger
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 from web.app.routes import note_bp
 from web.app.error_handlers import ErrorHandler
 
@@ -12,6 +14,12 @@ def make_app():
                    static_folder='../static', 
                    template_folder="../templates")
         app.secret_key = os.getenv("SECRET_KEY", "fallback_secret_key")
+        Limiter(
+            get_remote_address,
+            app=app,
+            default_limits=["3 per second"],
+            headers_enabled=True
+        )
         ErrorHandler(app)
         app.register_blueprint(note_bp)
         return app
