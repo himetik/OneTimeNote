23e227f9d5aa7a4e8360ce6e54acf71bf3942578
himetik <himetik@tutamail.com>
2025-01-14 23:54:46 +0300

    .

--- web/app/database.py
+++ web/app/database.py
@@ -1,12 +1,11 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from sqlalchemy.orm import sessionmaker, Session
 from typing import Generator
 from web.app.config import DATABASE_URL
 
 
 engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-Base = declarative_base()
 
 
 def get_db() -> Generator[Session, None, None]:
