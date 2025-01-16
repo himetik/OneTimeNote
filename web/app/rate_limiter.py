1e751c02a4e556af25daac6be05f18b5edbb7ccc
himetik <himetik@tutamail.com>
2025-01-16 09:33:51 +0300

    Refactor

--- web/app/rate_limiter.py
+++ web/app/rate_limiter.py
@@ -1,13 +1,13 @@
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from web.app.config import REDIS_URL
+from web.app.config import REDIS_URL, RATE_LIMITS
 
 
 def configure_limiter(app):
     Limiter(
         key_func=get_remote_address,
         app=app,
-        default_limits=["3 per second"],
+        default_limits=[RATE_LIMITS],
         storage_uri=REDIS_URL,
         headers_enabled=True
     )
